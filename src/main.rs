a44ec9e9101860efa6cf71f0dce16bfe95d638f6
Eemeli Syynimaa <eemeli.syynimaa@kapsi.fi>
2024-01-02 14:35:53 +0200

    Read file contents

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let path = "assets/part1/listing_0037_single_register_mov";
+    let data: Vec<u8> = fs::read(path).unwrap();
+    println!("Contents of file \"{}\":\n{:?}", path, data);
 }
