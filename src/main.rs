fcb28128f7ff1b046c6028e1e9f6b5718bda14ef
Eemeli Syynimaa <eemeli.syynimaa@kapsi.fi>
2024-01-02 16:17:49 +0200

    Parse command from the first byte

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::env;
 use std::fs;
 
+const MOV: u8 = 0b100010;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("Arguments: {:?}", args);
@@ -9,5 +11,14 @@ fn main() {
         let path = args[1].as_str();
         let data: Vec<u8> = fs::read(path).unwrap();
         println!("Contents of file \"{}\":\n{:?}", path, data);
+
+        let command: u8 = data[0] >> 2;
+
+        if command == MOV {
+            println!(
+                "Command is MOV: {:b} vs {:b} (from {:b}",
+                command, MOV, data[0]
+            );
+        }
     }
 }
