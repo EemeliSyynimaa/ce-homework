1766d4f4605c15e02ac31f4eda2915cef50959d5
Eemeli Syynimaa <eemeli.syynimaa@kapsi.fi>
2024-01-19 16:57:11 +0200

    Support more bytes

--- src/lib.rs
+++ src/lib.rs
@@ -22,16 +22,16 @@ const MOV: u8 = 0b100010;
 const REG0: [&str; 8] = ["al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"];
 const REG1: [&str; 8] = ["ax", "cx", "dx", "bx", "sp", "bp", "si", "di"];
 
-fn process_mov(command: u8, data: u8) {
+fn process_mov(command: u8, data: &[u8]) {
     // println!("; {:b} {:b}", command, data);
 
-    let m: u8 = (data & (128 + 64)) >> 6;
+    let m: u8 = (data[0] & (128 + 64)) >> 6;
 
     if m == 3 {
         let d: u8 = command & 2;
         let w: u8 = command & 1;
-        let reg: u8 = (data & (32 + 16 + 8)) >> 3;
-        let rm: u8 = data & (4 + 2 + 1);
+        let reg: u8 = (data[0] & (32 + 16 + 8)) >> 3;
+        let rm: u8 = data[0] & (4 + 2 + 1);
 
         let src: u8 = if d == 0 { reg } else { rm };
         let dst: u8 = if d == 0 { rm } else { reg };
@@ -50,12 +50,11 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     println!("bits 16\n");
     for i in 0..data.len() / 2 {
         let c_idx: usize = i * 2;
-        let b_idx: usize = c_idx + 1;
 
         let command: u8 = data[c_idx] >> 2;
 
         if command == MOV {
-            process_mov(data[c_idx], data[b_idx]);
+            process_mov(data[c_idx], &data[1..data.len()]);
         }
     }
 
